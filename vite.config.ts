622507aaed1f4a730a4b71d0d2fccf2863fb2f17
Min <solunar@vip.qq.com>
2024-10-27 20:07:30 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +15,4 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-})
+});
