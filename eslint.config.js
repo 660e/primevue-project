4a184cf08d7931136bf8264539ff3e6e03685b89
Min <solunar@vip.qq.com>
2024-10-27 21:00:46 +0800

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -7,21 +7,50 @@ export default [
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs['flat/recommended'],
   ...vueTsEslintConfig(),
   skipFormatting,
 
-  // https://eslint.org/docs/latest/rules/
   {
     rules: {
+      // https://eslint.org/docs/latest/rules/
       'no-var': 'error',
       'prefer-const': 'error',
+
+      // https://eslint.vuejs.org/rules/
+      'vue/attributes-order': [
+        'error',
+        {
+          order: [
+            'DEFINITION', // is, v-is
+            'LIST_RENDERING', // v-for
+            'CONDITIONALS', // v-if, v-else-if, v-else, v-show, v-cloak
+            'TWO_WAY_BINDING', // v-model
+            'RENDER_MODIFIERS', // v-once, v-pre
+            'CONTENT', // v-text, v-html
+            'SLOT', // slot, v-slot
+            'OTHER_DIRECTIVES', // v-custom-directive
+            'ATTR_DYNAMIC', // v-bind:prop, :prop
+            'EVENTS', // @click, v-on
+            'ATTR_STATIC', // prop, custom-prop
+            'GLOBAL', // id
+            'UNIQUE', // ref, key
+            'ATTR_SHORTHAND_BOOL', // boolean-prop
+          ],
+          alphabetical: true,
+        },
+      ],
+      'vue/custom-event-name-casing': ['error', 'kebab-case'],
+      'vue/html-self-closing': ['error', { html: { normal: 'never', void: 'always' } }],
+      'vue/max-attributes-per-line': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-static-inline-styles': ['error'],
+      'vue/singleline-html-element-content-newline': 'off',
     },
   },
 ];
