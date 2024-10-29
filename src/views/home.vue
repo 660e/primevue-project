c5cf4e37b8ae7896f63862c94ba5430e0297f660
Min <solunar@vip.qq.com>
2024-10-29 21:22:20 +0800

    Update

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,11 +3,18 @@ const value = ref();
 </script>
 
 <template>
-  <div class="h-screen flex flex-col justify-center items-center gap-2">
+  <div class="h-screen flex flex-col justify-center items-center gap-4">
     <div class="flex gap-2">
       <InputText v-model="value" type="text" />
       <Button class="!px-8" label="Submit" />
+      <div class="w-20 bg-primary rounded-border transition-colors hover:bg-primary-emphasis"></div>
     </div>
-    <div class="h-8 w-1/2 bg-primary rounded-border transition-colors hover:bg-primary-emphasis"></div>
+    <div class="flex gap-2">
+      <i class="pi pi-check"></i>
+      <i class="pi pi-times"></i>
+      <i class="pi pi-spin pi-cog"></i>
+      <span class="pi pi-search"></span>
+      <span class="pi pi-user"></span>
+    </div>
   </div>
 </template>
